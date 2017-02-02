bfbc72c15682dbf9db38ee29c4bb058888117cb4
Naresh Miriyala <naresh.miriyala@jemena.com>
2017-02-02 11:10:31 +1100

    adding comment

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -1,6 +1,6 @@
 (function() {
   'use strict';
-
+  // remove playerRatingsMock when using real service for player ratings
   angular
     .module('AflTableApp', ['ngAnimate', 'ngResource', 'ngRoute', 'ui.bootstrap', 'toastr','playerRatingsMock']);
 
